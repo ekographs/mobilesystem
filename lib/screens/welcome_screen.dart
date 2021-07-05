55a5abbee10451603fa0e4d02e2bc96cd2f3995a
ekographs <nwaforlorekoate@hotmail.com>
2021-07-06 03:29:08 +0400

    welcome screen done

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,30 +10,45 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
+        child: Stack(
           children: [
-            Expanded(child: OnBoardScreen()),
-            Text('Excited to order from your nearest store?', style: TextStyle(fontSize: 18,) ),
-            SizedBox(height: 30,),
-            TextButton(
-            style: TextButton.styleFrom(
-            backgroundColor: Colors.blueAccent,
-            alignment: Alignment.center,
-            primary: Colors.white,
-            textStyle: const TextStyle(fontSize: 30),
-            ),
-            onPressed: () {},
-              child: const Text('Set Delivery Location'),
-        ),
-            SizedBox(height: 30,),
-            RichText(text: TextSpan(
-            text: 'Returning Customer?', style: TextStyle(fontSize: 18,color: Colors.black),
-              children: [
-              TextSpan(
-                text: 'Login here',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+            Positioned(
+              right: 0.0,
+              top: 20.0,
+              child: TextButton(
+                child: Text('SKIP',style: TextStyle(color: Colors.blueAccent),),
+                onPressed: (){},
               ),
-            ],
             ),
+            Column(
+              children: [
+                Expanded(child: OnBoardScreen(),),
+                Text('Excited to order from your nearest store?', style: TextStyle(fontSize: 18,) ),
+                SizedBox(height: 30,),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    alignment: Alignment.center,
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 30),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Set Delivery Location',style: TextStyle(color: Colors.white),),
+                ),
+                SizedBox(height: 30,),
+                TextButton(
+                  child: RichText(text: TextSpan(
+                    text: 'Returning Customer?', style: TextStyle(fontSize: 18,color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: 'Login here',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+                      ),
+                    ],
+                  ),
+                  ),
+                  onPressed: (){},
+                ),
+              ],
             ),
           ],
         ),
